d8e93f649045ffcda5cf385dbf706a0005a85aa3
DoeringChristian <doering.christian.2001@gmx.de>
2017-11-08 08:52:43 +0100

    comments01

--- vectord.cpp
+++ vectord.cpp
@@ -26,6 +26,7 @@ double& Vectord::operator[](unsigned int index) const{
 }
 
 Vectord Vectord::operator *(const Matrixd &m) const{
+    //multiplication vector*matrix
     Vectord v(m.getHeight());
     for(int i = 0;i < m.getHeight();i++){
         double d = 0;
@@ -37,6 +38,7 @@ Vectord Vectord::operator *(const Matrixd &m) const{
 }
 
 void Vectord::operator =(const Vectord &v){
+    //= operator to copy the values of the pointer-array
     delete [] vect;
     this->length = v.size();
     this->vect = new double[length];
@@ -53,6 +55,7 @@ double sig(double d){
     return 1/(1+pow(M_E,d));
 }
 
+//sigmoid function for each element in a given vector
 Vectord sig(const Vectord &s){
     Vectord v(s.size());
     for(int i = 0;i < s.size();i++)
