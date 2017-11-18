1435f198e903c2ff7951fcc69b38709fe9d686ce
DoeringChristian <doering.christian.2001@gmx.de>
2017-11-18 11:10:30 +0100

    trainer to class.

--- trainer.cpp
+++ trainer.cpp
@@ -11,7 +11,7 @@ Trainer::Trainer(Network n, double randomness, unsigned int population){
     this->randomness = randomness;
     this->n = new Network[population];
     this->length = population;
-    for(int i = 0;i < population;i++)
+    for(uint i = 0;i < population;i++)
         this->n[i] = n;
     this->currentNet = 0;
     randomize(randomness);
@@ -21,8 +21,19 @@ Trainer::~Trainer(){
     delete [] n;
 }
 
+Network &Trainer::operator[](uint index) const{
+    return n[index];
+}
+
+uint Trainer::size() const{
+    return this->length;
+}
+
+Network &Trainer::current() const{
+    return n[currentNet];
+}
 void Trainer::randomize(double randomness){
-    for(int i = 1;i < this->length;i++)
+    for(uint i = 1;i < this->length;i++)
         n[i].randomize(randomness);           
 }
 
@@ -30,10 +41,10 @@ Network &Trainer::update(double fitness,double randomness){
     this->n[currentNet].setFitness(fitness);
     if(currentNet >= length){
         Network *tmp = &n[0];
-        for(int i = 0;i < length;i++)
+        for(uint i = 0;i < length;i++)
             if(n[i].getFitness() < tmp->getFitness())
                 tmp = &n[i];
-        for(int i = 0;i < length;i++)
+        for(uint i = 0;i < length;i++)
             this->n[i] = *tmp;
         this->currentNet = 0;
         randomize(randomness);
@@ -44,7 +55,9 @@ Network &Trainer::update(double fitness,double randomness){
     
 }
 
-
+void Trainer::setRandomness(double randomness){
+    this->randomness = randomness;
+}
 
 
 
--- trainer.h
+++ trainer.h
@@ -7,16 +7,21 @@
 
 using namespace std;
 
-struct Trainer{
+class Trainer{
     Network *n;
     unsigned int length;
     double randomness;
     unsigned int currentNet;
+public:
     Trainer();
     Trainer(Network n,double randomness,unsigned int population = 5);
     ~Trainer();
+    Network &operator[](uint index) const;
+    uint size() const;
+    Network &current() const;
     Network &update(double fitness, double randomness);
     void randomize(double randomness);
+    void setRandomness(double randomness);
 };
 
 #endif // TRAINER_H
