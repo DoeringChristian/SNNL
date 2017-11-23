eacc13e5b97de47153765ba8e0a9dc0b43f88881
DoeringChristian <doering.christian.2001@gmx.de>
2017-11-23 18:00:42 +0100

    bugfix02

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "trainer.h"
+#include "network.h"
 #include <cstdlib>
 #include <math.h>
 #include <ctime>
@@ -11,6 +11,15 @@ int main(){
     unsigned int t[3] = {2,3,2};
     Network n(t,3);
     Trainer tr(n,0.01);
-    cout << (((double)rand() / (double)(RAND_MAX))*2-1)*0.01 << endl;
+    Network n2;
+    n2 = tr.update(1,0.001);
+    n2.setInput(1,10);
+    while(true){
+        for(int i = 0;i < 1000;i++){
+            n2.update();
+            cout << tr.currentNet << "|" << i << "|" << n2.getOutput()[0] << "|" << n2.getOutput()[1] << endl;
+        }
+        n2 = tr.update(1,0.001);
+    }
     return 0;
 }
